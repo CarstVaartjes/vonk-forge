b7f7f78ce4013fcd4e81127c88217200444fa7d5
Carst Vaartjes <cvaartjes@visualfabriq.com>
2026-08-11 12:03:14 +0200

    fix(control): expose agent runtime identity (#63)

--- src/cluster_profiles/generated_control/models/agent_summary.py
+++ src/cluster_profiles/generated_control/models/agent_summary.py
@@ -32,8 +32,13 @@ class AgentSummary:
             node_id (str):
             stale (bool):
             state (str):
+            active_slot (Union[None, Unset, str]):
+            agent_sha256 (Union[None, Unset, str]):
+            build_digest (Union[None, Unset, str]):
             last_seen_age_seconds (Union[None, Unset, float]):
+            platform_version (Union[None, Unset, str]):
             protocol_version (Union[None, Unset, int]):
+            supervisor_generation (Union[None, Unset, int]):
      """
 
     agent_implementation: str
@@ -44,8 +49,13 @@ class AgentSummary:
     node_id: str
     stale: bool
     state: str
+    active_slot: Union[None, Unset, str] = UNSET
+    agent_sha256: Union[None, Unset, str] = UNSET
+    build_digest: Union[None, Unset, str] = UNSET
     last_seen_age_seconds: Union[None, Unset, float] = UNSET
+    platform_version: Union[None, Unset, str] = UNSET
     protocol_version: Union[None, Unset, int] = UNSET
+    supervisor_generation: Union[None, Unset, int] = UNSET
 
 
 
@@ -72,17 +82,47 @@ class AgentSummary:
 
         state = self.state
 
+        active_slot: Union[None, Unset, str]
+        if isinstance(self.active_slot, Unset):
+            active_slot = UNSET
+        else:
+            active_slot = self.active_slot
+
+        agent_sha256: Union[None, Unset, str]
+        if isinstance(self.agent_sha256, Unset):
+            agent_sha256 = UNSET
+        else:
+            agent_sha256 = self.agent_sha256
+
+        build_digest: Union[None, Unset, str]
+        if isinstance(self.build_digest, Unset):
+            build_digest = UNSET
+        else:
+            build_digest = self.build_digest
+
         last_seen_age_seconds: Union[None, Unset, float]
         if isinstance(self.last_seen_age_seconds, Unset):
             last_seen_age_seconds = UNSET
         else:
             last_seen_age_seconds = self.last_seen_age_seconds
 
+        platform_version: Union[None, Unset, str]
+        if isinstance(self.platform_version, Unset):
+            platform_version = UNSET
+        else:
+            platform_version = self.platform_version
+
         protocol_version: Union[None, Unset, int]
         if isinstance(self.protocol_version, Unset):
             protocol_version = UNSET
         else:
             protocol_version = self.protocol_version
+
+        supervisor_generation: Union[None, Unset, int]
+        if isinstance(self.supervisor_generation, Unset):
+            supervisor_generation = UNSET
+        else:
+            supervisor_generation = self.supervisor_generation
 
 
         field_dict: dict[str, Any] = {}
@@ -97,10 +137,20 @@ class AgentSummary:
             "stale": stale,
             "state": state,
         })
+        if active_slot is not UNSET:
+            field_dict["active_slot"] = active_slot
+        if agent_sha256 is not UNSET:
+            field_dict["agent_sha256"] = agent_sha256
+        if build_digest is not UNSET:
+            field_dict["build_digest"] = build_digest
         if last_seen_age_seconds is not UNSET:
             field_dict["last_seen_age_seconds"] = last_seen_age_seconds
+        if platform_version is not UNSET:
+            field_dict["platform_version"] = platform_version
         if protocol_version is not UNSET:
             field_dict["protocol_version"] = protocol_version
+        if supervisor_generation is not UNSET:
+            field_dict["supervisor_generation"] = supervisor_generation
 
         return field_dict
 
@@ -138,6 +188,36 @@ class AgentSummary:
 
         state = d.pop("state")
 
+        def _parse_active_slot(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        active_slot = _parse_active_slot(d.pop("active_slot", UNSET))
+
+
+        def _parse_agent_sha256(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        agent_sha256 = _parse_agent_sha256(d.pop("agent_sha256", UNSET))
+
+
+        def _parse_build_digest(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        build_digest = _parse_build_digest(d.pop("build_digest", UNSET))
+
+
         def _parse_last_seen_age_seconds(data: object) -> Union[None, Unset, float]:
             if data is None:
                 return data
@@ -146,6 +226,16 @@ class AgentSummary:
             return cast(Union[None, Unset, float], data)
 
         last_seen_age_seconds = _parse_last_seen_age_seconds(d.pop("last_seen_age_seconds", UNSET))
+
+
+        def _parse_platform_version(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        platform_version = _parse_platform_version(d.pop("platform_version", UNSET))
 
 
         def _parse_protocol_version(data: object) -> Union[None, Unset, int]:
@@ -158,6 +248,16 @@ class AgentSummary:
         protocol_version = _parse_protocol_version(d.pop("protocol_version", UNSET))
 
 
+        def _parse_supervisor_generation(data: object) -> Union[None, Unset, int]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, int], data)
+
+        supervisor_generation = _parse_supervisor_generation(d.pop("supervisor_generation", UNSET))
+
+
         agent_summary = cls(
             agent_implementation=agent_implementation,
             capabilities=capabilities,
@@ -167,8 +267,13 @@ class AgentSummary:
             node_id=node_id,
             stale=stale,
             state=state,
+            active_slot=active_slot,
+            agent_sha256=agent_sha256,
+            build_digest=build_digest,
             last_seen_age_seconds=last_seen_age_seconds,
+            platform_version=platform_version,
             protocol_version=protocol_version,
+            supervisor_generation=supervisor_generation,
         )
 
         return agent_summary
